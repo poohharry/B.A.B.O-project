cafe71df1e8993c85a68e667c7e3dd64b61477de
poohharry <poohharry@naver.com>
2022-05-25 21:58:28 +0900

    UserDAO에서 insertUser 메소드 작성중

--- main/java/common/UserDAO.java
+++ main/java/common/UserDAO.java
@@ -1,5 +1,8 @@
 package common;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class UserDAO {
 	
 	private DBConnectionMgr pool;
@@ -10,5 +13,24 @@ public class UserDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public boolean insertUser(UserVO vo) {
+		boolean flag = false;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			con = pool.getConnection();
+			// sql문 들어갈 자리
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		
+		return flag;
 	}
 }
