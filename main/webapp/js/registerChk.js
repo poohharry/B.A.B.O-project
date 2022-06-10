b10ad8af8d0fcaf8515618ae5812e0b8a586a082
sbs6363@naver.com <sbs6363@naver.com>
2022-06-10 12:29:11 +0900

    no message

--- main/webapp/js/registerChk.js
+++ main/webapp/js/registerChk.js
@@ -44,6 +44,24 @@ function signUpChk() {
 		return
 		
 	}
+	const month = sFrm.month.value;
+	if(sFrm.birth-dd.value >31){
+		alert("일은 31일까지 밖에 입력이 안됩니다.")
+	}
+	if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
+		if(sFrm.birth-dd >30){
+			alert("해당 달은 30일까지 있습니다.");
+			sFrm.birth-dd.focus();
+			return;
+		}
+	}
+	if( month == 2){
+		if(sFrm.birth-dd.value >28){
+			alert("2월은 28일까지 밖에 없습니다.");
+			sFrm.birth-dd.focus();
+			return;
+		}
+	}
 	if(sFrm.birth-dd.value==""){
 		alert("태어난 일을  입력해주세요.");
 		sFrm.birth-dd.focus();
