db81695307da62145da6aa7943ba0bdfe8d9a059
poohharry <poohharry@naver.com>
2022-06-14 17:54:01 +0900

    주석추가

--- main/java/common/writeServlet.java
+++ main/java/common/writeServlet.java
@@ -9,11 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 
 //@WebServlet("/common/write")
 public class writeServlet extends HttpServlet {
-
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		PostDAO dao = new PostDAO();
 		dao.writePost(request);
+		// request.getParameter(form의 input필드의 name태그)
+		// request안에 숨어있는 무언가를 찾아서 Redirect를 잡아줘야함
 		response.sendRedirect("Free_board.jsp");
 	}
 
