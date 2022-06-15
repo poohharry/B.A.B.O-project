570543682b6557244469a4ccab73d74a3e5747b8
poohharry <poohharry@naver.com>
2022-06-15 11:51:39 +0900

    수정

--- main/java/common/PostDAO.java
+++ main/java/common/PostDAO.java
@@ -18,8 +18,7 @@ public class PostDAO {
 	
 	// 사용자마다 현재 프로젝트의 경로가 다르기 때문에 그걸 미리 구해놓고 파일 업로드 경로를 상대적으로 바꿔준다
 	private static String path = (System.getProperty("user.dir")).replace("\\", "/");
-
-	private static final String SAVEFOLDER = path + "/src/main/webapp/uploadFiles/";
+	private static final String SAVEFOLDER = path + "/Project1/FirstProject/src/main/webapp/uploadFiles/";
 	private static final String ENCTYPE = "UTF-8";
 	private static int MAXSIZE = 10*1024*1024;
 	
@@ -80,7 +79,7 @@ public class PostDAO {
 			pstmt.setInt(1, pNum);
 			rs = pstmt.executeQuery();
 			if(rs.next()) {
-				vo.setPNum(rs.getInt("pNUm"));
+				vo.setPNum(rs.getInt("pNum"));
 				vo.setCategory(rs.getString("category"));
 				vo.setTitle(rs.getString("title"));
 				vo.setWritter(rs.getString("writter"));
